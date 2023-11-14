1cd26b1528bfb2411bf55569de3e5b18d03b6f51
AndreXMTNZ <andres04m@outlook.com>
2023-11-13 19:01:43 -0600

    retoques

--- Matematica V8/Form10.Designer.cs
+++ Matematica V8/Form10.Designer.cs
@@ -28,13 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlanoC));
             SuspendLayout();
             // 
             // PlanoC
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1262, 713);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1264, 721);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "PlanoC";
             Text = "Plano Cartesiano";
             ResumeLayout(false);
